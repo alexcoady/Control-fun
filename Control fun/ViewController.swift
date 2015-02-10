e34cd1b8d0f78d5e3a45a1f93b6b503ca4faf556
Alex Coady <alexandercoady@me.com>
2015-02-10 08:32:38 +0000

    Initial commit

--- Control fun/ViewController.swift
+++ Control fun/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Start slider label at correct value (dynamically ;])
+        sliderLabel.text = "\(lroundf(slider.value))"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func backgroundTap(sender: UIControl) {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        let progress = lroundf(sender.value)
+        sliderLabel.text = "\(progress)"
+    }
+    
 }
 
